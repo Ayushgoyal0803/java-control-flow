3dd6da64a435162b48c6f1e2b381407c94e2b763
Ayush <ayushgoyal0808@gmail.com>
2025-03-04 11:02:28 +0530

    [Ayush Goyal] ADD: Calculating factorial of a number

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Factorial{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number ");		
+		int n=sc.nextInt();
+		int fact=1;
+		int num=n;
+		while(num>0){
+			fact*=num;
+			num--;
+		}
+		System.out.println("The factorial of number "+n+" is "+fact);
+	}
+}
